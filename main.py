ed8e4997bb0915f9becd1e2cf038c888c1192cf0
sapperEduardo <166638298+sapperEduardo@users.noreply.github.com>
2024-11-06 15:11:35 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers.alumnos import alumno
 from routers.materias import materia
@@ -9,6 +10,22 @@ from db.session import Base, engine
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Lista de orígenes permitidos
+origins = [
+    "http://127.0.0.1:5500",  # Permite tu origen de desarrollo
+    "http://localhost:5500",  # Alternativa por si usas localhost
+    # Añade otros orígenes si los necesitas
+]
+
+# Configura el middleware CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],            # Permitir los orígenes especificados
+    allow_credentials=True,            # Permitir el uso de cookies
+    allow_methods=["*"],               # Permitir todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],               # Permitir todos los encabezados
+)
 
 @app.get("/")
 def read_root():
